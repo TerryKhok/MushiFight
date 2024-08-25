cd100e73456624cda2769f04353a6a40fae32e81
TerryKhok <diresnake@hotmail.com>
2024-08-25 15:41:46 +0900

    キャラ選択画面とリザルト画面

---
+++ Assets/MainSystem/MenuExpandShrink.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuExpandShrink : MonoBehaviour
+{
+    [SerializeField] GameObject _uiObject;
+    [SerializeField] float _scaleRate;
+    float _scaleNumber = 0;
+    bool _reverse = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!_reverse)
+        {
+            _scaleNumber = _scaleNumber + _scaleRate;
+            _uiObject.transform.localScale = new Vector3(1 + _scaleNumber, 1 + _scaleNumber, 1 + _scaleNumber);
+            if (_scaleNumber >= .20f)
+            {
+                _reverse = !_reverse;
+            }
+        }
+        else
+        {
+            _scaleNumber = _scaleNumber - _scaleRate;
+            _uiObject.transform.localScale = new Vector3(1 + _scaleNumber, 1 + _scaleNumber, 1 + _scaleNumber);
+            if (_scaleNumber <= 0f)
+            {
+                _reverse = !_reverse;
+            }
+        }
+    }
+}
